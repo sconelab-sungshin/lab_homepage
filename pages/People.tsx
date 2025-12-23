7e6a9afd1960d544e352b9ecb11093765fc119c1
songaeun <55118501+songaeun@users.noreply.github.com>
2025-12-23 12:53:14 +0900

    Update People.tsx

--- pages/People.tsx
+++ pages/People.tsx
@@ -39,7 +39,7 @@ const labMembers = [
     name: "송아리영",
     role: "Research Assistant",
     email: "student3@sungshin.ac.kr",
-    image: "/images/lab_logo_preliminary.png",
+    image: `${import.meta.env.BASE_URL}images/lab_logo_preliminary.png`,
   },
 ];
 
