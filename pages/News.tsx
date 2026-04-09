69df6ed5738676f91b5ac6c6ca870e4b1f4c8d84
songaeun <55118501+songaeun@users.noreply.github.com>
2026-04-09 15:35:34 +0900

    Update News.tsx

--- pages/News.tsx
+++ pages/News.tsx
@@ -7,6 +7,13 @@ import { NewsItem } from "../types";
 
 const newsData: NewsItem[] = [
   /* 새로운 뉴스 생길 때마다, 제일 위에 얹기 */
+    {
+    id: "0003",
+    date: "Mar 2026",
+    title: "Perceptual Hystersis for Scene Categories",
+    content: "A paper co-authored by Gaeun has been published in Journal of Experimental Psychology: Human Perception and Performance, Congrats!!",
+    url: "https://psycnet.apa.org/record/2027-49165-001",
+  },
   {
     id: "0002",
     date: "Feb 2026",
